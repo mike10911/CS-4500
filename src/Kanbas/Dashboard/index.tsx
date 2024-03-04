40566377b6100f0c3ed15484303153b07536c776
mike10911 <mikeymundia@gmail.com>
2024-03-04 17:02:12 -0500

    lab 4 setup and other fixes

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { courses } from "../Database";
+
+const courseCount = courses.length;
+
 function Dashboard() {
   return (
     <div className="p-4">
       <h1>Dashboard</h1> <hr />
-      <h2>Published Courses (12)</h2> <hr />
+      <h2>Published Courses ({courseCount}) </h2> <hr />
       <div className="row">
         <div className="row row-cols-1 row-cols-md-5 g-4">
           {courses.map((course) => (
--- src/Nav.tsx
+++ src/Nav.tsx
@@ -5,6 +5,8 @@ function Nav() {
     <nav className="nav nav-tabs mt-2">
       <Link to="/Labs/a3"
             className={`nav-link ${pathname.includes("a3") ? "active" : ""}`}>Assigment 3 Lab</Link>
+      <Link to="/Labs/a4"
+            className={`nav-link ${pathname.includes("a4") ? "active" : ""}`}>Assigment 4 Lab</Link>
       <Link to="/Kanbas"
             className={`nav-link ${pathname.includes("Kanbas") ? "active" : ""}`}>React Kanbas</Link>
       <Link to="/hello"
