7b3efb2b2fedda1289c6a6c77002470e63f02b55
mike10911 <mikeymundia@gmail.com>
2024-03-06 15:31:53 -0500

    lab 4 done

--- src/Labs/a4/index.tsx
+++ src/Labs/a4/index.tsx
@@ -1,10 +1,33 @@
-import React from "react";
+import ClickEvent from "./ClickEvent";
+import EventObject from "./EventObject";
+import Counter from "./Counter";
+import BooleanStateVariables from "./BooleanStateVariables";
+import StringStateVariables from "./StringStateVariables";
+import DateStateVariable from "./DateStateVariable";
+import ObjectStateVariable from "./ObjectStateVariable";
+import ArrayStateVariable from "./ArrayStateVariable";
+import ParentStateComponent from "./ParentStateComponent";
+import ReduxExamples from "./ReduxExamples";
+import TodoList from "./ReduxExamples/todos/TodoList";
 
 const Assignment4 = () => {
   return(
-    <>
+    <div className="container">
       <h1>Assignment 4</h1>
-    </>
+      <TodoList/>
+      <ClickEvent/>
+      <EventObject/>
+      <Counter/>
+      <BooleanStateVariables/>
+      <StringStateVariables/>
+      <DateStateVariable/>
+      <ObjectStateVariable/>
+      <ArrayStateVariable/>
+      <ParentStateComponent/>
+      <ReduxExamples/>
+      
+
+    </div>
   );
 };
 export default Assignment4;
--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,17 +1,21 @@
+import { Provider } from "react-redux";
 import Nav from "../Nav";
 import Assignment3 from "./a3";
 import Assignment4 from "./a4";
 import { Routes, Route, Navigate } from "react-router";
+import store from "./store";
 function Labs() {
   return (
+    <Provider store={store}>
     <div>
       <Nav />
       <Routes>
-        <Route path="/" element={<Navigate to="a3" />} />
+        <Route path="/" element={<Navigate to="a4" />} />
         <Route path="a3" element={<Assignment3 />} />
         <Route path="a4" element={<Assignment4 />} />
       </Routes>
     </div>
+  </Provider>
   );
 }
 export default Labs;
