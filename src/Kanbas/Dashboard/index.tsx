4b4a19b9e5a487069bb2c5415304cbf99e381e40
mike10911 <mikeymundia@gmail.com>
2024-03-06 16:49:16 -0500

    3.1 done

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -1,51 +1,133 @@
-import React from "react";
+import React, { useState } from "react";
+
 import { Link } from "react-router-dom";
 import { courses } from "../Database";
 
-const courseCount = courses.length;
-
 function Dashboard() {
-  return (
-    <div className="p-4">
-      <h1>Dashboard</h1> <hr />
-      <h2>Published Courses ({courseCount}) </h2> <hr />
-      <div className="row">
-        <div className="row row-cols-1 row-cols-md-5 g-4">
-          {courses.map((course) => (
-            <div key={course._id} className="col" style={{ width: 350 }}>
-              <div className="card">
-                <img
-                  src={`${course.image}`}
-                  className="card-img-top"
-                  style={{ height: 150 }}
-                  alt=""
-                />
-                <div className="card-body">
-                  <Link
-                    className="card-title"
-                    to={`/Kanbas/Courses/${course._id}/Home`}
-                    style={{
-                      textDecoration: "none",
-                      color: "navy",
-                      fontWeight: "bold",
-                    }}
-                  >
-                    {course.name}{" "}
-                  </Link>
-                  <p className="card-text">{course.name}</p>
-                  <Link
-                    to={`/Kanbas/Courses/${course._id}/Home`}
-                    className="btn btn-primary"
-                  >
-                    View Course{" "}
-                  </Link>
+    // TODO: update to be `courses`
+    const [courseList, setCourseList] = useState(courses);
+    const courseCount = courseList.length;
+    const dummyCourse = {
+        _id: "0",
+        name: "New Course",
+        number: "1000",
+        startDate: "2023-09-10",
+        endDate: "2023-12-15",
+        image: "/images/cs.jpeg",
+    };
+    const [course, setCourse] = useState(dummyCourse);
+
+    const addNewCourse = () => {
+        const newCourse = { ...course, _id: new Date().getTime().toString() };
+        setCourseList([...courseList, { ...course, ...newCourse }]);
+    };
+    const deleteCourse = (courseId: string) => {
+        setCourseList(courseList.filter((course) => course._id !== courseId));
+    };
+    const updateCourse = () => {
+        setCourseList(
+            courses.map((c) => {
+                if (c._id === course._id) {
+                    return course;
+                } else {
+                    return c;
+                }
+            })
+        );
+    };
+
+    return (
+        <div className="p-4">
+            <h1>Dashboard</h1> <hr />
+            <h5>Course</h5>
+            <input
+                value={course.name}
+                className="form-control"
+                onChange={(e) => setCourse({ ...course, name: e.target.value })}
+            />
+            <input
+                value={course.number}
+                className="form-control"
+                onChange={(e) =>
+                    setCourse({ ...course, number: e.target.value })
+                }
+            />
+            <input
+                value={course.startDate}
+                className="form-control"
+                type="date"
+                onChange={(e) =>
+                    setCourse({ ...course, startDate: e.target.value })
+                }
+            />
+            <input
+                value={course.endDate}
+                className="form-control"
+                type="date"
+                onChange={(e) =>
+                    setCourse({ ...course, endDate: e.target.value })
+                }
+            />
+            <button onClick={addNewCourse}>Add</button>
+            <button onClick={updateCourse}>Update</button>
+            <h2>Published Courses ({courseCount}) </h2> <hr />
+            <div className="row">
+                <div className="row row-cols-1 row-cols-md-5 g-4">
+                    {courseList.map((course) => (
+                        <div
+                            key={course._id}
+                            className="col"
+                            style={{ width: 350 }}
+                        >
+                            <div className="card">
+                                <img
+                                    src={`${course.image}`}
+                                    className="card-img-top"
+                                    style={{ height: 150 }}
+                                    alt=""
+                                />
+                                <div className="card-body">
+                                    <Link
+                                        className="card-title"
+                                        to={`/Kanbas/Courses/${course._id}/Home`}
+                                        style={{
+                                            textDecoration: "none",
+                                            color: "navy",
+                                            fontWeight: "bold",
+                                        }}
+                                    >
+                                        {course.name}{" "}
+                                        <button
+                                            onClick={(event) => {
+                                                event.preventDefault();
+                                                setCourse(course);
+                                            }}
+                                        >
+                                            Edit
+                                        </button>
+                                        <button
+                                            onClick={(event) => {
+                                                event.preventDefault();
+                                                deleteCourse(course._id);
+                                            }}
+                                        >
+                                            Delete
+                                        </button>
+                                    </Link>
+                                    <p className="card-text">{course.name}</p>
+                                    <Link
+                                        to={`/Kanbas/Courses/${course._id}/Home`}
+                                        className="btn btn-primary"
+                                    >
+                                        View Course{" "}
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
                 </div>
-              </div>
             </div>
-          ))}
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 export default Dashboard;
